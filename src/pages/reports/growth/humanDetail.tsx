146b5854a9322b2ed699328ced3ef8257c4c47c4
gnhweb <gemini19840314@gmail.com>
2026-08-30 11:28:52 +0900

    feat: add recovery control to growth detail

---
+++ src/pages/reports/growth/humanDetail.tsx
@@ -0,0 +1,4 @@
+import GrowthRecordDetail from './page';
+import ReportOwnerActions from '../ownerActions';
+import { useParams } from 'react-router-dom';
+export default function HumanGrowthDetail(){const {id}=useParams();return <><GrowthRecordDetail/><ReportOwnerActions reportType="growth" reportId={id||''}/></>}
