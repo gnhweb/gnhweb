27b5ba0f34dfbbf91d2d6e86fd6c5a69c0b59482
gnhweb <gemini19840314@gmail.com>
2026-08-30 11:25:48 +0900

    feat: extend teacher dashboard with approved report archive

---
+++ src/pages/teacherDashboard/enhancedPage.tsx
@@ -0,0 +1,11 @@
+import TeacherDashboard from './page';
+import ApprovedReportsPanel from './approvedReportsPanel';
+
+export default function TeacherDashboardEnhanced() {
+  return (
+    <>
+      <TeacherDashboard />
+      <ApprovedReportsPanel />
+    </>
+  );
+}
